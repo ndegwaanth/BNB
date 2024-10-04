22b706baafdd9df02b2c1bb881455a38204ca583
Anthony <ndegwaanthony300@gmail.com>
2024-10-04 21:44:29 +0300

    initial commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""app.py to link API"""
 import os
 from flask import Flask
 from models import storage
@@ -11,16 +11,20 @@ app = Flask(__name__)
 # Import blueprint app_views and register it to the Flask instance app
 app.register_blueprint(app_views)
 
+
 # Define a method to handle teardown_appcontext
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Close the storage on teardown."""
     storage.close()
 
-if __name__ == "__main__":
-    # Get host and port from environment variables or use default values
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
 
-    # Run the Flask server
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
